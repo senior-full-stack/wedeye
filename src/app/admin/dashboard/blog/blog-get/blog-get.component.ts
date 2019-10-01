a34818efd189c300058982e2485653f8b7ae05ef
senior-full-stack <web.expert007@hotmail.com>
2019-10-02 01:18:03 +0800

    complete static page

--- src/app/admin/dashboard/blog/blog-get/blog-get.component.ts
+++ src/app/admin/dashboard/blog/blog-get/blog-get.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from "@angular/core";
+import { HttpEventType, HttpResponse } from "@angular/common/http";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-blog-get',
-  templateUrl: './blog-get.component.html',
-  styleUrls: ['./blog-get.component.scss']
+  selector: "app-blog-get",
+  templateUrl: "./blog-get.component.html",
+  styleUrls: ["./blog-get.component.scss"]
 })
 export class BlogGetComponent implements OnInit {
 
-  constructor() { }
+  downLoading = false;
+  fileUrl;
+  
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
+    const data = "some text";
+
+    const blob = new Blob([data], { type: "application/octet-stream" });
+
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      window.URL.createObjectURL(blob)
+    );
   }
 
+  download() {}
 }
